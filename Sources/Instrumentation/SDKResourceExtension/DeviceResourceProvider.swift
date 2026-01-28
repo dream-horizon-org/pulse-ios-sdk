1cc098af349f2a82bedc59ff9ccd272e1571eebe
Hemant Gehlot <hemant.gehlot@dream11.com>
2026-01-28 19:50:55 +0530

    Adds resource attributes to achieve parity with Android SDK

--- Sources/Instrumentation/SDKResourceExtension/DeviceResourceProvider.swift
+++ Sources/Instrumentation/SDKResourceExtension/DeviceResourceProvider.swift
@@ -4,9 +4,13 @@
  */
 
 import Foundation
-
 import OpenTelemetryApi
 import OpenTelemetrySdk
+#if os(iOS) || os(tvOS)
+import UIKit
+#elseif os(watchOS)
+import WatchKit
+#endif
 
 public class DeviceResourceProvider: ResourceProvider {
   let deviceSource: IDeviceDataSource
@@ -25,6 +29,21 @@ public class DeviceResourceProvider: ResourceProvider {
     if let deviceId = deviceSource.identifier {
       attributes[ResourceAttributes.deviceId.rawValue] = AttributeValue.string(deviceId)
     }
+    
+    // device.manufacturer (official ResourceAttributes - iOS apps SHOULD hardcode "Apple")
+    // OpenTelemetry spec: https://opentelemetry.io/docs/specs/semconv/resource/device/#manufacturer
+    #if os(iOS) || os(tvOS) || os(watchOS) || os(macOS)
+    attributes[ResourceAttributes.deviceManufacturer.rawValue] = AttributeValue.string("Apple")
+    #endif
+    
+    // device.model.name (official ResourceAttributes - user-friendly model name)
+    // UIDevice.model: https://developer.apple.com/documentation/uikit/uidevice
+    #if os(iOS) || os(tvOS)
+    attributes[ResourceAttributes.deviceModelName.rawValue] = AttributeValue.string(UIDevice.current.model)
+    #elseif os(watchOS)
+    // WKInterfaceDevice.model: https://developer.apple.com/documentation/watchkit/wkinterfacedevice
+    attributes[ResourceAttributes.deviceModelName.rawValue] = AttributeValue.string(WKInterfaceDevice.current().model)
+    #endif
 
     return attributes
   }
