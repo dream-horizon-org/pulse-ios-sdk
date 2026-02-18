bcd81f8024c92399d46aeafcc8ea10c6a843eab1
Devanshj11 <devansh.j@dream11.com>
2026-02-18 08:28:42 +0530

    feat: tenant support in ios (#75)
    * feat: tenant support in ios
    
    * chore: minor logging update in example app
    
    * chore: changed the tenant specific attributes and keys
    
    * chore: minor name changes for tenant support parity
    
    * chore: small naming change in variables for consistency

--- Sources/PulseKit/PulseKit.swift
+++ Sources/PulseKit/PulseKit.swift
@@ -73,6 +73,7 @@ public class PulseKit {
 
     public func initialize(
         endpointBaseUrl: String,
+        projectId: String,
         endpointHeaders: [String: String]? = nil,
         globalAttributes: [String: AttributeValue]? = nil,
         resource: ((inout [String: AttributeValue]) -> Void)? = nil,
@@ -94,11 +95,15 @@ public class PulseKit {
             var config = InstrumentationConfiguration()
             instrumentations?(&config)
 
-            let resource = buildResource(resource: resource)
+            // Merge ProjectID  header with user-provided headers
+            let projectIdHeader = [PulseAttributes.projectIdHeaderKey: projectId]
+            let endpointHeadersWithProjectID = (endpointHeaders ?? [:]).merging(projectIdHeader) { _, new in new }
+
+            let resource = buildResource(projectId: projectId, resource: resource)
 
             let (tracerProvider, loggerProvider, openTelemetry) = buildOpenTelemetrySDK(
                 endpointBaseUrl: endpointBaseUrl,
-                endpointHeaders: endpointHeaders,
+                endpointHeaders: endpointHeadersWithProjectID,
                 resource: resource,
                 config: config,
                 tracerProviderCustomizer: tracerProviderCustomizer,
@@ -134,12 +139,13 @@ public class PulseKit {
 
     // MARK: - Private Helper Methods
 
-    private func buildResource(resource: ((inout [String: AttributeValue]) -> Void)?) -> Resource {
+    private func buildResource(projectId: String, resource: ((inout [String: AttributeValue]) -> Void)?) -> Resource {
         let defaultResource = DefaultResources().get()
         
         var attributes = defaultResource.attributes
         
         attributes[ResourceAttributes.telemetrySdkName.rawValue] = AttributeValue.string(PulseAttributes.PulseSdkNames.iosSwift)
+        attributes[PulseAttributes.projectId] = AttributeValue.string(projectId)
         
         if let resourceCustomizer = resource {
             resourceCustomizer(&attributes)
